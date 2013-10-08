a87a6b6f3136819fb8bb7da7aa9186abdafd0930
Aaron Allport <aaronallport@unknown.home>
2013-10-08 20:53:37 +0100

    Removing jQuery from left-nav-flyout and nav-toggle nav patterns

--- js/nav-patterns/nav-toggle.js
+++ js/nav-patterns/nav-toggle.js
@@ -3,20 +3,59 @@
 // No need to thouch this block of code, only the initialisation line below
 var NavigationToggle = {
 	init: function(menu, menuLink, toggleClass) {
-		$('body').addClass('js');
-		var $menu = $(menu),
-		$menulink = $(menuLink);
+		this.addClass(document.querySelector("body"), "js");
+		
+		var $menu = document.querySelector(menu);
+		var $menulink = document.querySelector(menuLink);
 
-		$menulink.click(function() {
-			$menulink.toggleClass(toggleClass);
-			$menu.toggleClass(toggleClass);
+		this.addEventListener($menuLink, "click", function() {
+			NavigationToggle.toggleClass($menulink, toggleClass);
+			NavigationToggle.toggleClass($menu, toggleClass);
+
 			return false;
 		});
+	},
+
+	addEventListener: function(element, eventName, handler) {
+		if (element.addEventListener) {
+			element.addEventListener(eventName, handler, false);
+		}
+		else if (element.attachEvent) {
+			element.attachEvent('on' + eventName, handler);
+		}
+		else {
+			element['on' + eventName] = handler;
+		}
+	},
+
+	hasClass: function (elem, className) {
+    	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    },
+
+    addClass: function(elem, className) {
+	    if (!hasClass(elem, className)) {
+	        elem.className += ' ' + className;
+	    }
+	},
+
+    toggleClass: function(elem, className) {
+	    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
+
+	    if (hasClass(elem, className)) {
+	        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+	            newClass = newClass.replace( ' ' + className + ' ' , ' ' );
+	        }
+
+	        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+	    } 
+	    else {
+	        elem.className += ' ' + className;
+	    }
 	}
 };
 
 // START HERE!!
-$(document).ready(function() {
+window.onload = function() {
 	// Here is where you need to edit. Arguments are as follows:
 	// menu - The menu item (if it is referenced by ID, prefix with "#". Classes require a ".")
 	// menuLink - The menulink item (if it is referenced by ID, prefix with "#". Classes require a ".")
@@ -26,4 +65,4 @@ $(document).ready(function() {
 	// NavigationToggle.init(menu, menuLink, toggleClass);
 
 	NavigationToggle.init("#navigation-toggle", ".navigation-toggle", "active");
-});
+};
